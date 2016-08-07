94fb06824722b94a78440c1eb4d25f1d03bed1cd
David Souther <davidsouther@gmail.com>
2016-08-07 17:26:28 -0400

    Load specs via systemjs

---
+++ src/shared/hero.spec.ts
@@ -0,0 +1,15 @@
+import {Hero} from './hero';
+
+const HERO_NAME = 'Super Cat';
+
+export const root = describe('Hero model', () => {
+  it('has a name', () => {
+    const hero: Hero = {id: 1, name: HERO_NAME};
+    expect(hero.name).toBe(HERO_NAME);
+  });
+
+  it('has an id', () => {
+    const hero: Hero = {id: 1, name: HERO_NAME};
+    expect(hero.id).toBe(1);
+  })
+});
